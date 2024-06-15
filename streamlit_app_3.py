0b356b652e5c2001cc9c1aa49fa7acdbc7abe11c
NisarAhmad1981 <30634781+NisarAhmad1981@users.noreply.github.com>
2024-06-15 08:41:37 +0530

    Update streamlit_app_3.py

--- streamlit_app_3.py
+++ streamlit_app_3.py
@@ -1,10 +1,11 @@
 # Import python packages
 import streamlit as st
+import requests
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
-connections=st.connection("snowflake")
-session=connections.session()
+cnx=st.connection("snowflake")
+session=cnx.session()
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie:cup_with_straw:")
